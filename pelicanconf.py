eafc4bc623d5cbb70fed495c2ae134992546b397
Hisanobu Nakamura <hisanobu@pop-os.localdomain>
2023-09-21 18:07:06 +0900

    renewal

---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+AUTHOR = 'hissanova'
+SITENAME = "Hissanova's Blog"
+# SITEURL = "http://hissanova.gihub.io"
+SITETITLE = "Hissanova's Blog"
+SITESUBTITLE = "沖縄んかい暮らちょーる<br>キジムナーやさ!!"
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = 'ja'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('沖縄語辞典アプリ', 'https://okinawago.app'),
+)
+
+# Social widget
+SOCIAL = (
+    ('twitter', 'https://twitter.com/hissanova'),
+    ('soundcloud', 'https://soundcloud.com/hissanova'),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = "Flex"
